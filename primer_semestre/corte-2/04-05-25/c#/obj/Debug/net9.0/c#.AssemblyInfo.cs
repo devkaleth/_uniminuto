a04a86238f16b5b9dd59e828cfadeae47b710c31
devkaleth <dev.kaleth.mendoza@gmail.com>
2025-04-23 10:29:27 -0500

    [ v2 - README]

--- primer_semestre/corte-2/04-05-25/c#/obj/Debug/net9.0/c#.AssemblyInfo.cs
+++ primer_semestre/corte-2/04-05-25/c#/obj/Debug/net9.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1990dadaca8934b83e107d4aae26f565fc19d0d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9154b1cc5c213961b35a3935e6d49d06f6bcb582")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
